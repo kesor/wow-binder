8a386b808961e8df1cadd4f1721bfcacc09ef6d1
Evgeny Zislis <evgeny.zislis@gmail.com>
2011-04-19 07:54:22 +0300

    make binder actually work

--- Binder.lua
+++ Binder.lua
@@ -7,107 +7,103 @@ local bframe = "Binder_VirtualMacro"
 SLASH_UI_ERRORS_ON1 = "/erron"
 SLASH_UI_ERRORS_OFF1 = "/erroff"
 
-function Binder:MapMacro(macro_name, body)
-  -- creates a button for this macro
+function Binder:MapMacro(macro_name, macro_body)
+  -- creates a button attribute for a macro
+  -- later can be used in /click or a CLICK bind
   local bname = "b" .. string.gsub(macro_name, " ", "_")
-  self.widget:SetAttribute("type-"..bname, "macro")
-  self.widget:SetAttribute("macrotext-"..bname, body)
+  Binder.widget:SetAttribute("type-"..bname, "macro")
+  Binder.widget:SetAttribute("macrotext-"..bname, macro_body)
 end
 
-function Binder:MapKey(key, macro_name)
+function Binder:LoadMacros()
+  if (self.global and self.global['macros']) then
+    for macro_name, macro_body in pairs(self.global['macros']) do
+      self:MapMacro(macro_name, macro_body)
+    end
+  end
+  if (self[self.class] and self[self.class]['macros']) then
+    for macro_name, macro_body in pairs(self[Binder.class]['macros']) do
+      self:MapMacro(macro_name, macro_body)
+    end
+  end
+  if self.talent then
+    if (self[self.class][self.talent .. '_macros']) then
+      for macro_name, macro_body in pairs(self[self.class][self.talent .. '_macros']) do
+        self:MapMacro(macro_name, macro_body)
+      end
+    end
+  end
+end
+
+function Binder:MapKey(key, action)
   -- clicks on a button for this macro
-  local bname = "b" .. string.gsub(macro_name, " ", "_")
+  local bname = "b" .. string.gsub(action, " ", "_")
   local keys = GetBindingKey("CLICK " .. bframe .. ":" .. bname)
   if not keys then
     SetOverrideBindingClick(self.eventframe, false, key, bframe, bname)
   end
 end
 
-function Binder:WhoAmI()
-  _, self.race = string.lower( UnitRace("player") )
-  _, self.class = string.lower( UnitClass("player") )
-  _, self.talent = string.lower( GetTalentTabInfo(GetPrimaryTalentTree(false,false,GetActiveTalentGroup())) )
-  return { self.race, self.class, self.talent }
-end
-
-function Binder:LoadMacros()
-  if (self.global and self.global['macros']) then
-    for macro_name, macro_body in pairs(self.global['macros']) do
-      self.MapMacro(macro_name, macro_body)
-    end
-  end
-  if (self[self.class] and self[self.class]['macros']) then
-    for macro_name, macro_body in pairs(self[self.class]['macros']) do
-      self.MapMacro(macro_name, macro_body)
-    end
-  end
-  if (self[self.class][self.talent .. '_macros']) then
-    for macro_name, macro_body in pairs(self[self.class][self.talent .. '_macros']) do
-      self.MapMacro(macro_name, macro_body)
-    end
-  end
-end
-
 function Binder:LoadKeys()
-  -- Racial
-  -- TODO:
-  -- Binder.racial[Binder.race]   = [m1.."Z"],
-
-  if (self.global and self.global['keybinds']) then
-    -- special case, keys/actions are backwards
-    for keybind_key, keybind_action in pairs(self.global['keybinds']) do
-      self.MapKey(keybind_name, keybind_body)
+  -- global
+  if (self.global) then
+    if (self.global['keybinds']) then
+      for key, command in pairs(self.global['keybinds']) do
+        SetOverrideBinding(self.eventframe, false, key, command)
+      end
+    end
+    if (self.global["modclicks"]) then
+      for action, binding in pairs(self.global["modclicks"]) do
+        SetModifiedClick( action, binding )
+      end
     end
   end
+  -- racial
+  if (self.racial and self.racial[self.race]) then
+    -- Hardcoded Racial ability for (m1 + Z) key
+    SetOverrideBindingSpell(self.eventframe, false, self.m1.."Z", self.racial[self.race])
+  end
+  -- class
   if (self[self.class] and self[self.class]['keybinds']) then
-    for keybind_name, keybind_body in pairs(self[self.class]['keybinds']) do
-      self.MapKey(keybind_name, keybind_body)
+    for spell, attrs in pairs(self[self.class]['keybinds']) do
+      SetOverrideBindingSpell(self.eventframe, false, attrs['key'], spell)
     end
   end
-  if (self[self.class][self.talent .. '_keybinds']) then
-    for keybind_name, keybind_body in pairs(self[self.class][self.talent .. '_keybinds']) do
-      self.MapKey(keybind_name, keybind_body)
-    end
-  end
-
-
-  -- Global keybinds
-  if (Binder.global and Binder.global['keybinds']) then
-  end
-
-  if (Binder[race] and Binder[race]['keybinds']) then
-  end
-
-  if self.spell_keybinds then
-    for key, macro in pairs(self.macro_keybinds) do
-
-      if self.macros[macro] then
-        self:MapMacro(key, macro, self.macros[macro])
-      else
-        print("No such macro " .. macro .. " defined!")
+  -- talent
+  if self.talent then
+    if (self[self.class][self.talent .. '_keybinds']) then
+      for spell, attrs in pairs(self[self.class][self.talent .. '_keybinds']) do
+        SetOverrideBindingSpell(self.eventframe, false, attrs['key'], spell)
       end
     end
   end
-  if self.macro_keybinds then
-    for key, macro in pairs(self.macro_keybinds) do
-      if self.macros[macro] then
-        self:MapMacro(key, macro, self.macros[macro])
-      else
-        print("No such macro " .. macro .. " defined!")
-      end
-    end
-  end
+end
+
+function Binder:WhoAmI()
+  local _, race = UnitRace("player")
+  self.race = string.lower(race)
+  local _, class = UnitClass("player")
+  self.class = string.lower(class)
+  local talent_group = GetActiveTalentGroup()
+  local talent_tree = GetPrimaryTalentTree(false, false, talent_group)
+  local _, talent = GetTalentTabInfo(talent_tree)
+  self.talent = string.lower(talent)
+
+  local whoami = self.race .. ' ' .. self.class .. ' ' .. self.talent
+  print("Loading Binder for: " .. whoami)
+
+  return { self.race, self.class, self.talent }
 end
 
 function Binder:init()
   self.eventframe = CreateFrame("Frame", eframe, UIParent)
   -- self.widget : a button widget that saves the macro bindings on itself
   self.widget = CreateFrame("Button", bframe, UIParent, "SecureActionButtonTemplate")
-  self.eventframe:RegisterEvent("PLAYER_ALIVE")
+  self.eventframe:RegisterEvent("VARIABLES_LOADED")
+
   self.eventframe:SetScript("OnEvent",
     function(...)
       self:WhoAmI()
-      print("Loading Binder for: " .. strjoin(' / ', unpack(self.WhoAmI())))
       self:LoadMacros()
       self:LoadKeys()
     end
@@ -116,3 +112,4 @@ end
 
 Binder:init()
 Binder.init = nil
+

28b1b73a8ae4e4a9aa5f2e593680bd0da614abc2
Evgeny Zislis <evgeny.zislis@gmail.com>
2011-04-20 20:40:01 +0300

    use a smart function to bind keys and register on PLAYER_ALIVE when talent data is available

--- Binder.lua
+++ Binder.lua
@@ -36,12 +36,23 @@ function Binder:LoadMacros()
 end
 
 function Binder:MapKey(key, action)
-  -- clicks on a button for this macro
+
   local bname = "b" .. string.gsub(action, " ", "_")
-  local keys = GetBindingKey("CLICK " .. bframe .. ":" .. bname)
-  if not keys then
+  if self.widget:GetAttribute("type-"..bname) == "macro" then
+    -- virtual macro
     SetOverrideBindingClick(self.eventframe, false, key, bframe, bname)
+    return
   end
+
+  name, _ = GetSpellInfo(action)
+  if name == action then
+    -- spell
+    SetOverrideBindingSpell(self.eventframe, false, key, action)
+    return
+  end
+
+  -- general key
+  SetOverrideBinding(self.eventframe, false, key, action)
 end
 
 function Binder:LoadKeys()
@@ -49,7 +60,14 @@ function Binder:LoadKeys()
   if (self.global) then
     if (self.global['keybinds']) then
       for key, command in pairs(self.global['keybinds']) do
-        SetOverrideBinding(self.eventframe, false, key, command)
+        -- SetOverrideBinding(self.eventframe, false, key, command)
+        self:MapKey(key,command)
+      end
+    end
+    if (self.global['macro_keybinds']) then
+      for spell, attrs in pairs(self.global['macro_keybinds']) do
+        -- SetOverrideBindingSpell(self.eventframe, false, attrs['key'], spell)
+        self:MapKey(attrs['key'], spell)
       end
     end
     if (self.global["modclicks"]) then
@@ -61,19 +79,22 @@ function Binder:LoadKeys()
   -- racial
   if (self.racial and self.racial[self.race]) then
     -- Hardcoded Racial ability for (m1 + Z) key
-    SetOverrideBindingSpell(self.eventframe, false, self.m1.."Z", self.racial[self.race])
+    -- SetOverrideBindingSpell(self.eventframe, false, self.m1.."Z", self.racial[self.race])
+    self:MapKey(self.m1.."Z",self.racial[self.race])
   end
   -- class
   if (self[self.class] and self[self.class]['keybinds']) then
     for spell, attrs in pairs(self[self.class]['keybinds']) do
-      SetOverrideBindingSpell(self.eventframe, false, attrs['key'], spell)
+      -- SetOverrideBindingSpell(self.eventframe, false, attrs['key'], spell)
+      self:MapKey(attrs['key'], spell)
     end
   end
   -- talent
   if self.talent then
     if (self[self.class][self.talent .. '_keybinds']) then
       for spell, attrs in pairs(self[self.class][self.talent .. '_keybinds']) do
-        SetOverrideBindingSpell(self.eventframe, false, attrs['key'], spell)
+        -- SetOverrideBindingSpell(self.eventframe, false, attrs['key'], spell)
+        self:MapKey(attrs['key'], spell)
       end
     end
   end
@@ -99,7 +120,8 @@ function Binder:init()
   self.eventframe = CreateFrame("Frame", eframe, UIParent)
   -- self.widget : a button widget that saves the macro bindings on itself
   self.widget = CreateFrame("Button", bframe, UIParent, "SecureActionButtonTemplate")
-  self.eventframe:RegisterEvent("VARIABLES_LOADED")
+  -- self.eventframe:RegisterEvent("VARIABLES_LOADED")
+  self.eventframe:RegisterEvent("PLAYER_ALIVE")
 
   self.eventframe:SetScript("OnEvent",
     function(...)

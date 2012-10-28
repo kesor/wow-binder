26783a9d470968db9b089e1aa331c39e83740f43
Evgeny Zislis <evgeny.zislis@gmail.com>
2012-10-28 20:33:36 +0200

    add monk class

---
+++ Monk.lua
@@ -0,0 +1,17 @@
+local _, Binder = ...
+
+local m1 = Binder.m1 -- 'CTRL-'
+local m2 = Binder.m2 -- 'SHIFT-'
+
+Binder.monk = {}
+
+Binder.monk["macros"] = {
+}
+Binder.monk["keybinds"] = {
+}
+Binder.monk["brewmaster macros"] = {
+}
+Binder.monk["mistweaver keybinds"] = {
+}
+Binder.monk["windwalker keybinds"] = {
+}
